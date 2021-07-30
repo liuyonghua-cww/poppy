92d579b5c9a347f6530fd4a4a021bbbd9eb2b4e8
刘永华 <liuyonghua@ganweisoft.com>
2021-07-30 18:03:35 +0800

    feat:新增事件

---
+++ learn-x6/src/events/mouse.js
@@ -0,0 +1,30 @@
+export class Mouse {
+    constructor(graph) {
+        this.graph = graph;
+    }
+    nodeMouseEnter() {
+        const { graph } = this;
+        const container = document.getElementById('x6_container');
+        graph.on('node:mouseenter', () => {
+            const ports = container.querySelectorAll(
+                '.x6-port-body',
+            );
+            this.showPorts(ports, true)
+        })
+    }
+    nodeMouseLeave() {
+        const { graph } = this;
+        const container = document.getElementById('x6_container');
+        graph.on('node:mouseleave', () => {
+            const ports = container.querySelectorAll(
+                '.x6-port-body',
+            );
+            this.showPorts(ports, false)
+        })
+    }
+    showPorts(ports, show) {
+        for (let i = 0, len = ports.length; i < len; i = i + 1) {
+            ports[i].style.visibility = show ? 'visible' : 'hidden'
+        }
+    }
+}
