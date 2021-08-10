2c2524f05779000054dd913ed5bea6906c8adc2c
刘永华 <liuyonghua@ganweisoft.com>
2021-08-10 13:18:56 +0800

    feat：新增工具栏相关操作

--- learn-x6/src/components/Header/ToolBar/index.vue
+++ learn-x6/src/components/Header/ToolBar/index.vue
@@ -1,6 +1,6 @@
 <template>
     <section id="toolbar">
-        <div v-for="(item, i) of toolBarOpt">
+        <div v-for="(item, i) of toolBarOpt" @click="handleClick(item.type)">
             <i :class="['iconfont', item.icon]"></i>
             <span>{{ item.label }}</span>
         </div>
@@ -9,11 +9,61 @@
 
 <script>
 import {toolBarOpt} from "./toolBarOpt";
+import { mapState } from "vuex";
 export default {
     name: "index",
+    computed: {
+        ...mapState('app', [
+           'graph'
+        ])
+    },
     data() {
         return {
-            toolBarOpt
+            toolBarOpt,
+            zoom: null
+        }
+    },
+    mounted() {
+        this.$nextTick(() => {
+            this.getZoom();
+            this.graph.on('scale', () => {
+                this.getZoom()
+            })
+        })
+    },
+    updated() {
+
+    },
+    methods: {
+        handleClick(type) {
+            const { graph } = this;
+            switch (type) {
+                case 'undo':
+                    graph.history.undo();
+                    break;
+                case 'redo':
+                    graph.history.redo();
+                    break;
+                case 'save':
+                    graph.toPNG()
+                    break;
+                case 'delete':
+                    graph.remove(graph.getSelectedCells())
+                    break;
+                case 'zoomout':
+                    if (this.zoom > 0.5) {
+                        graph.zoom(-0.1)
+                    }
+                    break;
+                case 'zoomin':
+                    if (this.zoom < 1.5) {
+                        graph.zoom(0.1)
+                    }
+                    break;
+            }
+        },
+        getZoom() {
+            this.zoom = this.graph.zoom()
         }
     }
 }
@@ -38,6 +88,7 @@ export default {
         }
         > span {
             height: 40%;
+            user-select: none;
         }
         &:hover {
             color: @primary-5;
--- learn-x6/src/components/Header/ToolBar/toolBarOpt.js
+++ learn-x6/src/components/Header/ToolBar/toolBarOpt.js
@@ -1,31 +1,31 @@
 export const toolBarOpt = [
     {
-        value: 'undo',
+        type: 'undo',
         label: '撤销',
         icon: 'icon-undo'
     },
     {
-        value: 'redo',
+        type: 'redo',
         label: '重做',
         icon: 'icon-redo'
     },
     {
-        value: 'save',
+        type: 'save',
         label: '保存',
         icon: 'icon-save'
     },
     {
-        value: 'delete',
+        type: 'delete',
         label: '删除',
         icon: 'icon-delete'
     },
     {
-        value: 'zoomout',
+        type: 'zoomout',
         label: '缩小',
         icon: 'icon-zoomout'
     },
     {
-        value: 'zoomin',
+        type: 'zoomin',
         label: '放大',
         icon: 'icon-zoomin'
     },
