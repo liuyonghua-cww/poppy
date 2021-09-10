dfb46812e673ec10dd9df0688c27dbd531493564
刘永华 <liuyonghua@ganweisoft.com>
2021-09-10 17:53:33 +0800

    feat：新增组件模板

---
+++ learn-x6/src/components/Contextmenu/index.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="contextmenu" :style="contextmenuStyle">
+        <a-card>
+            <a-card-grid class="grid" @click="up">
+                上移一层
+            </a-card-grid>
+            <a-card-grid class="grid" @click="down">
+                下移一层
+            </a-card-grid>
+            <a-card-grid class="grid" @click="setData">
+                数据设置
+            </a-card-grid>
+        </a-card>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "index",
+    props: {
+        contextmenuStyle: {
+            type: Object,
+            default: null
+        }
+    },
+    data() {
+        return {
+        }
+    },
+    methods: {
+        up(e) {
+            e.stopPropagation();
+            e.preventDefault();
+
+        },
+        down(e) {
+            e.stopPropagation();
+            e.preventDefault();
+
+        },
+        setData(e) {
+            e.stopPropagation();
+            e.preventDefault();
+
+        }
+    }
+};
+</script>
+
+<style scoped lang="less">
+.contextmenu {
+    position: fixed;
+    left: -99999px;
+    width: 150px;
+    box-shadow: 3px 3px 10px 0 rgba(0, 0, 0, 0.15) !important;
+    .grid {
+        width: 100%;
+        padding: 8px 8px 8px 20px;
+        cursor: pointer;
+    }
+}
+
+</style>
---
+++ learn-x6/src/components/Editor/MonacoEditor.vue
@@ -0,0 +1,23 @@
+<template>
+    <a-modal v-model="editorVisible" title="Basic Modal">
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+    </a-modal>
+</template>
+
+<script>
+export default {
+    name: "MonacoEditor",
+    props: {
+        editorVisible: {
+            type: Boolean,
+            default: false
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
