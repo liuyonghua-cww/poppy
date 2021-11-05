40812a34b783649e26a1396c38aef5a2a023ca47
刘永华 <liuyonghua@ganweisoft.com>
2021-11-05 18:00:47 +0800

    refactor：修改文件类型

--- learn-x6/src/readFile/readFile.js
+++ learn-x6/src/readFile/readFile.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 // 获取折线图初始化配置
-axios.get('echartsOption/line-chart.js').then(res => {
+axios.get('echartsOption/line-chart.txt').then(res => {
     window.lineChart = res.data;
 })
-axios.get('echartsOption/bar-chart.js').then(res => {
+axios.get('echartsOption/bar-chart.txt').then(res => {
     window.barChart = res.data;
 })
-axios.get('echartsOption/pie-chart.js').then(res => {
+axios.get('echartsOption/pie-chart.txt').then(res => {
     window.pieChart = res.data;
 });
-axios.get('echartsOption/liquid-fill.js').then(res => {
+axios.get('echartsOption/liquid-fill.txt').then(res => {
     window.liquidFill = res.data;
 })
